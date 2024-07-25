10df3fc2cd1be01b2cabad7fef4e0b978287081c
DeliriousIdea <63449281+DeliriousIdea@users.noreply.github.com>
2024-07-24 22:01:15 -0600

    Menus selected

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const menuIconRef = document.querySelector(".menu-icon")
+const menuRef = document.querySelector(".main-menu li")
 
+
+console.log(menuIconRef) // Make sure it's selected correctly
+console.log(menuRef) // Make sure it's selected correctly
+
+menuIconRef.addEventListener('click', toggleMenu)
+
+function toggleMenu(e) {
+    e.preventDefault()
+    console.log(menuRef.hasAttribute('visibility'))
+}
